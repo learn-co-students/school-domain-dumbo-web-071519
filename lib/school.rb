435bcb84cf8b56dd95478eadee35968f2c338daa
Ashna Shah-Grover <ashnashahgrover777@gmail.com>
2019-07-17 15:38:38 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+require 'pry'
+
+class School
+
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.map {|grade, students| [grade, students.sort]}.to_h
+  end
+
+end
+
+school = School.new("Bayside High School")
