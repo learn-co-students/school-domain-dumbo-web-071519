94c3d14112b18d9d48d264d14ca43943631c4717
Carlos Guzman <tomaffew@me.com>
2019-07-18 00:09:27 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+
+class School
+    def initialize(new_school)
+      @name = new_school
+      @roster = {}
+    end
+
+    def roster
+      @roster
+    end
+
+    def add_student(student_name, num)
+      if !(@roster.keys.include? num)
+        @roster[num] = [student_name]
+      else
+        @roster[num] << student_name
+      end
+    end
+
+    def grade(num_grade)
+      @roster[num_grade]
+    end
+
+    def sort
+      @roster.keys.reduce({}) do |memo, grade|
+        students = @roster[grade]
+        memo[grade] = students.sort
+        memo
+      end
+    end
+end
+
